5c1c844cb255ee22b6d611c2ff8a129661937093
johannesberger <xaaris@googlemail.com>
2021-02-21 23:05:37 +0100

    Added cognito for user management

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-central-1_ix552buBu', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '285078l0dgh7u2b807uo2a47au', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
